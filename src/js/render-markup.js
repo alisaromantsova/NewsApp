36763592ea47b351c71c26dc6128d1d1cdf61bf4
AniutkinyRukodelki <anyageiko91@gmail.com>
2023-03-22 19:04:05 +0200

    btn Add to favorite
    btn Add to favorite/ >Remove from favorite

--- src/js/render-markup.js
+++ src/js/render-markup.js
@@ -6,10 +6,16 @@ const markup = array.map((article)=>{
     <h2 class="news__title">${article.title}</h2>
        <p class="news__text">${article.text}</p>
      <img class="new__img" src="${ article.imgSrc}"/>
-  <a class="new__link" href="${article.url}">read more</a>
- </div>`;
-  }) .join('');
-div.insertAdjacentHTML('beforeend', markup);
+     <div class="article_flag">
+     <button class="article_flag--add"><span class="article_flag_text">Add to favorite</span>
+     </button>
+     <button class="article_flag--remove is-hidden"><span class="article_flag_text">Remove from favorite</span>
+     </button>
+     </div>
+     <a class="new__link" href="${article.url}">read more</a>
+     </div>`;
+}).join('');
+  div.insertAdjacentHTML('beforeend', markup);
 }
 
 
