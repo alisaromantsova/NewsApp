9c56f6a587d2e9b0a0d333152912cc66a69e2027
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-03-22 23:34:51 +0200

    add weather

--- src/js/fetches.js
+++ src/js/fetches.js
@@ -1,7 +1,7 @@
 import axios, { isCancel, AxiosError } from 'axios';
 const KEY = 'l8KIk4PAFV2Lgtto4JKKNaM7Q53Z5QMa';
-const div = document.querySelector('.news')
-
+const KEY_WEATHER_API = 'b45ce7b42bd659df434b4de28331d70c';
+const div = document.querySelector('.news');
 
 // Фетч популярних
 export async function fetchPopularNews() {
@@ -10,20 +10,20 @@ export async function fetchPopularNews() {
   );
 
   const popularNews = await response.data.results;
-  const array = popularNews.map((article)=>{
-    const meta = 'media-metadata'
-    const newsObject ={
+  const array = popularNews.map(article => {
+    const meta = 'media-metadata';
+    const newsObject = {
       title: article.title,
-      text:article.abstract,
-      imgSrc:article.media[0][meta][0].url ? article.media[0][meta][0].url : 'qwe',
-      link: article.url
-    }
-    return newsObject
-
-  })
-  return array
-  }
-
+      text: article.abstract,
+      imgSrc: article.media[0][meta][0].url
+        ? article.media[0][meta][0].url
+        : 'qwe',
+      link: article.url,
+    };
+    return newsObject;
+  });
+  return array;
+}
 
 //Фетч по категориям
 export async function fetchNewsByCategory(category) {
@@ -32,38 +32,44 @@ export async function fetchNewsByCategory(category) {
   );
 
   const newsByCategory = await response.data.results;
-  const array = newsByCategory.map((article)=>{
-    const newsObject ={
+  const array = newsByCategory.map(article => {
+    const newsObject = {
       title: article.title,
-      text:article.abstract,
-      imgSrc:article.multimedia[0] ? article.multimedia[0].url : 'qwe',
-      link: article.url
-    }
-    return newsObject
+      text: article.abstract,
+      imgSrc: article.multimedia[0] ? article.multimedia[0].url : 'qwe',
+      link: article.url,
+    };
+    return newsObject;
+  });
+  return array;
+}
 
-  })
-  return array
-  }
-
-
-
-//Фетч по поисковому запросу 
+//Фетч по поисковому запросу
 export async function fetchNewsBySearch(search) {
   const response = await axios.get(
     `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${KEY}`
   );
 
   const newsBySearch = await response.data.response.docs;
-  console.log(newsBySearch)
-  const array = newsBySearch.map((article)=>{
-    const newsObject ={
+  console.log(newsBySearch);
+  const array = newsBySearch.map(article => {
+    const newsObject = {
       title: article.headline.main,
-      text:article.abstract,
-      imgSrc:`https://static01.nyt.com/${article.multimedia[0].url ? article.multimedia[0].url : 'qwe'}`,
-      link: article.url
-    }
-    return newsObject
+      text: article.abstract,
+      imgSrc: `https://static01.nyt.com/${
+        article.multimedia[0].url ? article.multimedia[0].url : 'qwe'
+      }`,
+      link: article.url,
+    };
+    return newsObject;
+  });
+  return array;
+}
 
-  })
-  return array
-  }
+//  Запит на Weather API
+export async function getCurrentWeather(lat, lon) {
+  const products = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY_WEATHER_API}&units=metric`
+  );
+  return products;
+}
