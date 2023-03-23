0ceba284c40d9429a5a2d1b6822200a77a14420a
Diana Forostiana <dianaforost@gmail.com>
2023-03-23 13:19:32 +0100

    Adding class
    "light" or "dark"

--- src/js/theme-btn.js
+++ src/js/theme-btn.js
@@ -1,2 +1,15 @@
 import { fetchPopularNews, fetchNewsByCategory, fetchNewsBySearch } from './fetches';
 import {renderMarkup} from './render-markup'
+
+const spanEl = document.querySelector('.ball');
+spanEl.addEventListener('click', ChangeTheme);
+document.body.classList.add('light');
+function ChangeTheme(){
+    if(document.body.classList.contains('light')){
+        document.body.classList.remove('light');
+        document.body.classList.add('dark'); 
+    } else{
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    }
+};
