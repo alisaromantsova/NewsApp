6c2f7fc807bfa597f1ca2d754377a748b31805fe
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-03-24 00:48:19 +0200

    fix weather

--- src/js/onmount.js
+++ src/js/onmount.js
@@ -1,7 +1,14 @@
-import { fetchPopularNews, fetchNewsByCategory, fetchNewsBySearch, renderEmptyMarkup } from './fetches';
-import {renderMarkup} from './render-markup'
-onMount()
-async function onMount(){
-    const result = await fetchPopularNews()
-renderMarkup(result)
+import {
+  fetchPopularNews,
+  fetchNewsByCategory,
+  fetchNewsBySearch,
+  renderEmptyMarkup,
+} from './fetches';
+import { successCallback, failureCallback } from './weather';
+import { renderMarkup } from './render-markup';
+onMount();
+async function onMount() {
+  const result = await fetchPopularNews();
+  renderMarkup(result);
+  navigator.geolocation.getCurrentPosition(successCallback, failureCallback);
 }
