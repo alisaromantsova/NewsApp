269acfc525fc8c8ee26a7be49c1996ed3709e4bc
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-03-24 21:49:51 +0200

    bags

--- src/js/addtofavorite.js
+++ src/js/addtofavorite.js
@@ -2,10 +2,12 @@ const favoriteList = document.querySelector('.list-news');
 
 function renderMarkup() {
   const cards = JSON.parse(localStorage.getItem('newsCard'));
-  favoriteList.insertAdjacentHTML(
-    'afterend',
-    cards.map(card => card.newsCard)
-  );
+  if (cards) {
+    favoriteList.insertAdjacentHTML(
+      'afterend',
+      cards.map(card => card.newsCard)
+    );
+  }
 }
 
 renderMarkup();
