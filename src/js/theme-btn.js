cc5f602603c519394bdd814b031924da5f575fad
Diana Forostiana <dianaforost@gmail.com>
2023-03-24 11:41:18 +0100

    Stored in localStorage
    The topic value and input state are stored in localStorage

--- src/js/theme-btn.js
+++ src/js/theme-btn.js
@@ -4,23 +4,27 @@ import {renderMarkup} from './render-markup'
 const checkboxEl = document.querySelector('.chose-checkbox');
 checkboxEl.addEventListener('click', ChangeTheme);
 
-let activeTheme = localStorage.getItem('theme'); 
-document.body.classList.add(activeTheme);
-let activeCheckbox = localStorage.getItem('isChecked');
-console.log(activeCheckbox);
-// if(activeCheckbox === 'true'){
-    //     checkboxEl.setAttribute('checked', true);
-// } else{
-//     checkboxEl.removeAttribute('checked');
-// }
 
-// if(activeCheckbox === "true"){
-//     checkboxEl.setAttribute('checked', true);
-// }
+
+window.onload = function() {
+    let selectedTheme = localStorage.getItem('selectedTheme');
+    if (selectedTheme) {
+      document.body.classList.add(selectedTheme);
+    } else{
+        document.body.classList.add('light');
+    }
+  }
+
+
+let activeCheckbox = localStorage.getItem('isChecked');
+if(activeCheckbox === "true"){
+    checkboxEl.setAttribute('checked', true);
+}
+
+
+
 function ChangeTheme(){
-    //  else if(activeCheckbox === "false"){
-    //     checkboxEl.removeAttribute('checked');
-    // }
+
     if(document.body.classList.contains('light')){
         document.body.classList.remove('light');
         document.body.classList.add('dark'); 
@@ -28,14 +32,12 @@ function ChangeTheme(){
         document.body.classList.remove('dark');
         document.body.classList.add('light');
     }
-    let theme = document.body.getAttribute("class");
-    localStorage.setItem('theme',theme);
-    let isChecked = checkboxEl.checked;
-    // if(theme === "dark"){
-    //     isChecked = "true"
-    // } else if(theme === "light"){
-    //     isChecked = "false"
-    // }
-    localStorage.setItem('isChecked', isChecked);
 
+    let theme = document.body.getAttribute("class");
+    localStorage.setItem('selectedTheme',theme);
+
+    let isChecked = checkboxEl.checked;
+    localStorage.setItem('isChecked', isChecked);
 };
+
+
