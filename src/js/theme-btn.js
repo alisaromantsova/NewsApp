8d208fb85f98a7cab7d1265fdcc5f5dc03a118a8
Diana Forostiana <dianaforost@gmail.com>
2023-03-24 10:50:31 +0100

    Update theme-btn

--- src/js/theme-btn.js
+++ src/js/theme-btn.js
@@ -1,11 +1,26 @@
 import { fetchPopularNews, fetchNewsByCategory, fetchNewsBySearch } from './fetches';
 import {renderMarkup} from './render-markup'
 
-const spanEl = document.querySelector('.ball');
-spanEl.addEventListener('click', ChangeTheme);
+const checkboxEl = document.querySelector('.chose-checkbox');
+checkboxEl.addEventListener('click', ChangeTheme);
+
 let activeTheme = localStorage.getItem('theme'); 
 document.body.classList.add(activeTheme);
+let activeCheckbox = localStorage.getItem('isChecked');
+console.log(activeCheckbox);
+// if(activeCheckbox === 'true'){
+    //     checkboxEl.setAttribute('checked', true);
+// } else{
+//     checkboxEl.removeAttribute('checked');
+// }
+
+// if(activeCheckbox === "true"){
+//     checkboxEl.setAttribute('checked', true);
+// }
 function ChangeTheme(){
+    //  else if(activeCheckbox === "false"){
+    //     checkboxEl.removeAttribute('checked');
+    // }
     if(document.body.classList.contains('light')){
         document.body.classList.remove('light');
         document.body.classList.add('dark'); 
@@ -14,5 +29,13 @@ function ChangeTheme(){
         document.body.classList.add('light');
     }
     let theme = document.body.getAttribute("class");
-    localStorage.setItem('theme', theme);
+    localStorage.setItem('theme',theme);
+    let isChecked = checkboxEl.checked;
+    // if(theme === "dark"){
+    //     isChecked = "true"
+    // } else if(theme === "light"){
+    //     isChecked = "false"
+    // }
+    localStorage.setItem('isChecked', isChecked);
+
 };
