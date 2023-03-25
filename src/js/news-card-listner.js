ce20f9c5d2a954aa4d7b4f0ff332f4ea54d2a869
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-03-25 22:04:07 +0200

    Update news-card-listner.js

--- src/js/news-card-listner.js
+++ src/js/news-card-listner.js
@@ -1,22 +1,52 @@
 // збереження в локалсторедж по кліку на add
 
 const divClassNews = document.querySelector('.news');
-
 divClassNews.addEventListener('click', onAddToFavoriteClick);
-const arreyCard = JSON.parse(localStorage.getItem('newsCard'))
-  ? [...JSON.parse(localStorage.getItem('newsCard'))]
-  : [];
 
 function onAddToFavoriteClick(event) {
+  const arreyCard = JSON.parse(localStorage.getItem('newsCard'))
+    ? [...JSON.parse(localStorage.getItem('newsCard'))]
+    : [];
   if (event.target.tagName !== 'SPAN' && event.target.tagName !== 'BUTTON') {
     return;
   }
 
-  const newsCard =
+  let newsCard =
     event.target.tagName === 'BUTTON'
       ? event.target.parentNode.parentNode.innerHTML
       : event.target.parentNode.parentNode.parentNode.innerHTML;
 
-  arreyCard.push({ newsCard });
-  localStorage.setItem('newsCard', JSON.stringify(arreyCard));
+  const linkNewCArd =
+    event.target.tagName === 'BUTTON'
+      ? event.target.parentNode.parentNode.querySelector('.news__link')
+      : event.target.parentNode.parentNode.parentNode.querySelector(
+          '.news__link'
+        );
+
+  if (
+    event.target.classList.contains('news__removebtn') ||
+    event.target.parentNode.classList.contains('news__removebtn')
+  ) {
+    localStorage.removeItem('newsCard');
+    const arreyCardSecond = [];
+    arreyCard.map(item => {
+      if (item.newsCard.includes(linkNewCArd)) {
+      } else {
+        if (item) {
+          arreyCardSecond.push({ ...item });
+        }
+        return;
+      }
+    });
+
+    if (arreyCardSecond.length !== 0) {
+      localStorage.setItem('newsCard', JSON.stringify(arreyCardSecond));
+    }
+    return;
+  }
+
+  if (arreyCard !== []) {
+    arreyCard.push({ newsCard });
+    localStorage.setItem('newsCard', JSON.stringify(arreyCard));
+  }
 }
