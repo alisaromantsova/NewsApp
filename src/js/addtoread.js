fdcd65d2eb4516c8d674774cb016a73fac2733db
Klementiev Ivan <klivin76@gmail.com>
2023-03-27 01:09:38 +0300

    change local read

--- src/js/addtoread.js
+++ src/js/addtoread.js
@@ -6,49 +6,29 @@ function readMoreClick(event) {
     ? [...JSON.parse(localStorage.getItem('newsReadMore'))]
     : [];
 
-  if (
-    event.target.tagName !== 'A'
-  ) {
+  if (event.target.tagName !== 'A') {
     return;
   }
 
   const linkNewReadCard = event.target
-  .closest('.new__card')
-  .querySelector('.news__link');
+    .closest('.new__card')
+    .querySelector('.news__link');
 
   let newsReadMoreCard = event.target.closest('.new__card').innerHTML;
   if (arreyReadCard.length !== 0) {
-  arreyReadCard.map((item) => {
-    
-  newsReadMoreCard = item.newsReadMoreCard.includes(linkNewReadCard) ? 
-   0 : event.target.closest('.new__card').innerHTML;
-   console.log(newsReadMoreCard);
-  //  return newsReadMoreCard
-  })
+    // newsReadMoreCard = item.newsReadMoreCard.includes(linkNewReadCard) ?
+    //  0 : event.target.closest('.new__card').innerHTML;
+
+    arreyReadCard.map(item => {
+      if (item.newsReadMoreCard.includes(linkNewReadCard)) {
+        newsReadMoreCard = null;
+        return false;
+      }
+    });
   }
-  // newsReadMoreCard = event.target.closest('.new__card').innerHTML;
-//     let linkMore = [];
-//    if (arreyReadCard.length !== 0) {
-   
-//     arreyReadCard.map(item => {
-//       if (item.newsReadMoreCard.includes(linkNewReadCard)) {
-//           return;
-//  }
-//  else {
-//   linkMore.push({ newsReadMoreCard });
-//  }  
-//   })
-// linkMore = [];
-  
-//    } 
-//    if (linkMore.length === 0) {
-//     arreyReadCard.push({ newsReadMoreCard });
-//     localStorage.setItem('newsReadMore', JSON.stringify(arreyReadCard));
-//     return
-//    }   
-//    localStorage.setItem('newsReadMore', JSON.stringify(linkMore));
-if (newsReadMoreCard !== 0) {
-  arreyReadCard.push({ newsReadMoreCard });
-  localStorage.setItem('newsReadMore', JSON.stringify(arreyReadCard));
-}  
- }
+
+  if (newsReadMoreCard) {
+    arreyReadCard.push({ newsReadMoreCard });
+    localStorage.setItem('newsReadMore', JSON.stringify(arreyReadCard));
+  }
+}
