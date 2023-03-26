d45de8fdfbbb4c49d855af85a839721436de1854
Diana Forostiana <dianaforost@gmail.com>
2023-03-26 22:25:56 +0200

    Autocomplete OFF

--- src/js/search-field.js
+++ src/js/search-field.js
@@ -12,7 +12,7 @@ const selectEl = document.querySelector('.nav-select ');
 const div = document.querySelector('.news');
 
 searchForm.addEventListener('submit', createNews);
-
+searchForm.setAttribute('autocomplete', "off");
 function createNews(event) {
   event.preventDefault();
   const inputName = event.currentTarget.searchQuery.value.trim();
