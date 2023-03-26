44277709fae3473a5378b0597380928ebed38256
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-03-27 01:43:58 +0300

    check bags

--- src/favorite.js
+++ src/favorite.js
@@ -1,10 +1,10 @@
 import axios, { isCancel, AxiosError } from 'axios';
 // import './js/render-markup';
-import './js/read-page';
+import './js/addtofavorite';
 import './js/theme-btn';
 import './js/current-page-underline';
 import './js/search-field';
 import './js/mobile-menu';
 import './js/btnUpp';
 import './js/header-fixed-position';
-import './js/read-page';
+
