0c126dca9cd2e70ff619ed4efbc69bf776d57d5e
DmytriyGavryliuk <114095666+DmytriyGavryliuk@users.noreply.github.com>
2023-03-27 11:56:42 +0300

    Update onmount.js

--- src/js/onmount.js
+++ src/js/onmount.js
@@ -8,8 +8,7 @@ import { renderMarkup } from './render-markup';
 import { categoryValue, removeActiveClass } from './navigation';
 import { successCallback, failureCallback } from './weather';
 console.log(window);
-const date = new Date();
-console.log(date.getTime()); 
+ 
 onMount()
 async function onMount() {
   removeActiveClass();
