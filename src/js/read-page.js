ed361ebb2159edeaa301c368d71cf13f90bb175c
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-03-28 19:16:41 +0300

    read add

--- src/js/read-page.js
+++ src/js/read-page.js
@@ -5,7 +5,7 @@ import { setEventAfterRead } from './news-card';
 // const dateMerkup = document.querySelector('.label');
 // const readList = document.querySelector('.content');
 // const cardsRead = JSON.parse(localStorage.getItem('newsReadMore'));
-// readList.addEventListener('click', onRemoveNewCardToReadClick);
+
 // if (!cardsRead) {
 //   readList.innerHTML = renderEmptyMarkup();
 // }
@@ -32,47 +32,11 @@ function renderMarkup() {
 
 //renderMarkup();
 
-function onRemoveNewCardToReadClick(event) {
-  onAddToFavoriteClick(event); // функція додавання карток у фейворіт
-  //     event.target.tagName !== 'A'
-  //       ) {
-  //     return;
-  //   }
-  //   const arreyReadCard = JSON.parse(localStorage.getItem('newsReadMore'))
-  //     ? [...JSON.parse(localStorage.getItem('newsReadMore'))]
-  //     : [];
-  //   const linkNewReadCard = event.target
-  //     .closest('.new__card')
-  //     .querySelector('.news__link');
-  // if (arreyReadCard.length !== 0) {
-  //   localStorage.removeItem('newsReadMore');
-  //   const arreyCardSecond = [];
-  //   arreyCard.map(item => {
-  //     if (item.newsReadMore.includes(linkNewReadCard)) {
-  //       return;
-  //     } else {
-  //       if (item) {
-  //         arreyCardSecond.push({ ...item });
-  //       }
-  //     }
-  //   });
-  //     if (arreyCardSecond.length !== 0) {
-  //       localStorage.setItem('newsReadMore', JSON.stringify(arreyCardSecond));
-  //       readList.innerHTML = null;
-  //       readList.insertAdjacentHTML(
-  //         'afterbegin',
-  //         arreyCardSecond
-  //           .map(item => `<div class="new__card">${item.newsReadMore}</div>`)
-  //           .join('')
-  //       );
-  //     } else {
-  //       readList.innerHTML = renderEmptyMarkup();
-  //     }
-  //     return;
-  //   }
-}
 const containerRead = document.querySelector('.container__read');
-
+containerRead.addEventListener('click', onRemoveNewCardToReadClick);
+function onRemoveNewCardToReadClick(event) {
+  onAddToFavoriteClick(event);
+}
 let cards = JSON.parse(localStorage.getItem('newsReadMore'));
 let arrayForRendering = null;
 if (!cards) {
